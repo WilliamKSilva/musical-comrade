6ca0c79214372f190f9fbbd70b00d9e2b02f31bf
William <wilz.kelvin12@gmail.com>
2022-11-05 12:03:30 -0300

    refactor: add Game field relation in User entitie

--- server/domain/user.go
+++ server/domain/user.go
@@ -3,13 +3,12 @@ package domain
 import "time"
 
 type User struct {
-	Id        string    `gorm:"primaryKey" json:"id"`
-	Name      string    `json:"name"`
-	Email     string    `json:"email"`
-	Password  string    `json:"password"`
-	Nickname  string    `json:"nickname"`
-	Games     []Game    `gorm:"foreignKey:GameId"`
-	GameId    string    `json:"game_id"`
+	Id        string `gorm:"primaryKey" json:"id"`
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	Nickname  string `json:"nickname"`
+	Games     []Game
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
